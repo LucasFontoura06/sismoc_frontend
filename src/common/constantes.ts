b44c1119e4362c3cd831f593854728e7ae5303c7
DevLucasFontoura <devlucasfontoura@gmail.com>
2024-11-12 17:26:30 -0300

    Correcoes para build

--- src/common/constantes.ts
+++ src/common/constantes.ts
@@ -1,6 +1,6 @@
 export const CONSTANTES = {
   // API URLs
-  API_URL: "http://localhost:3001/sismoc/api",
+  API_URL: "https://sismoc-production-8jaa.vercel.app/",
   API_IBGE:
     "https://servicodados.ibge.gov.br/api/v1/localidades/estados/<UF>/municipios",
   API_CNAE_GRUPOS: "https://servicodados.ibge.gov.br/api/v2/cnae/grupos",
